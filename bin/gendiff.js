797c90b3cedacdf78aeae1399d378d0c6e81a095
Vladislav Mayakin <vlad99may@gmail.com>
2022-10-25 20:30:05 +0300

    Fix version (gendiff.js)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .version('9.4.1')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format');
