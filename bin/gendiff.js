ba599c798b907d957144233736066ad8a77c4b68
Vladislav Mayakin <vlad99may@gmail.com>
2022-10-27 22:20:48 +0300

    Correct linter`s remarks

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-import genDiff from '../src/index.js';
-import _ from 'lodash';
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
