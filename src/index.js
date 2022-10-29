91c60b3b01814da604b83be989f2ca98b5f01bae
Vladislav Mayakin <vlad99may@gmail.com>
2022-10-29 22:02:15 +0300

    Refactoring program logic code (index.js)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import _ from 'lodash';
+import parseData from './parsers.js';
 
 const genDiff = (file1, file2) => {
   const fileKeys1 = Object.keys(file1);
@@ -28,11 +29,14 @@ const genDiff = (file1, file2) => {
 };
 
 export default (filepath1, filepath2) => {
+  const fileFormat1 = path.extname(filepath1);
+  const fileFormat2 = path.extname(filepath2);
+
   const data1 = readFileSync(path.resolve(filepath1), 'utf-8');
   const data2 = readFileSync(path.resolve(filepath2), 'utf-8');
 
-  const parsedData1 = JSON.parse(data1);
-  const parsedData2 = JSON.parse(data2);
+  const parsedData1 = parseData(data1, fileFormat1);
+  const parsedData2 = parseData(data2, fileFormat2);
 
   return genDiff(parsedData1, parsedData2);
 };
