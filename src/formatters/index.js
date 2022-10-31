3f309a28ffa6350bb491b0290156deb693fa8f75
Vladislav Mayakin <vlad99may@gmail.com>
2022-10-31 23:20:25 +0300

    Add the code that selects the format to output

---
+++ src/formatters/index.js
@@ -0,0 +1,15 @@
+import stylish from './stylish.js';
+import plain from './plain.js';
+
+export default (data, format) => {
+  switch (format) {
+    case 'stylish':
+      return stylish(data);
+    case 'plain':
+      return plain(data);
+    case 'json':
+      return JSON.stringify(data);
+    default:
+      throw new Error(`${format} is not defined`);
+  }
+};
