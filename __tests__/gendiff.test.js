2fc87522666980ac07a585d96bf46d69c4f2aa0d
Vladislav Mayakin <vlad99may@gmail.com>
2022-11-04 17:50:38 +0300

    Refactoring tests v2

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,27 +10,14 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFixture = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8');
 
 test.each([
-  ['file1.json', 'file2.json'],
-  ['file1.json', 'file2.json', 'stylish'],
-  ['file1.json', 'file2.json', 'plain'],
-  ['file1.json', 'file2.json', 'json'],
-  ['file1.yml', 'file2.yml'],
-  ['file1.yml', 'file2.yml', 'stylish'],
-  ['file1.yml', 'file2.yml', 'plain'],
-  ['file1.yml', 'file2.yml', 'json'],
-])('genDiff(%#)', (file1, file2, format = 'stylish') => {
+  ['file1.json', 'file2.json', 'stylish', 'stylish.txt'],
+  ['file1.json', 'file2.json', 'plain', 'plain.txt'],
+  ['file1.json', 'file2.json', 'json', 'json.txt'],
+  ['file1.yml', 'file2.yml', 'stylish', 'stylish.txt'],
+  ['file1.yml', 'file2.yml', 'plain', 'plain.txt'],
+  ['file1.yml', 'file2.yml', 'json', 'json.txt'],
+])('genDiff(%#)', (file1, file2, format, expectedValue) => {
   const recieved = genDiff(getFixturePath(file1), getFixturePath(file2), format);
-  const expected = (formatter) => {
-    switch (formatter) {
-      case 'stylish':
-        return readFixture('stylish.txt');
-      case 'plain':
-        return readFixture('plain.txt');
-      case 'json':
-        return readFixture('json.txt');
-      default:
-        throw new Error(`${formatter} is not defined`);
-    }
-  };
-  expect(recieved).toBe(expected(format));
+  const expected = readFixture(expectedValue);
+  expect(recieved).toEqual(expected);
 });
